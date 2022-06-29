99289e076bbcf54f1ad17b505f672e03ad3571d3
Diala-Sleem <diala.slim@yahoo.com>
2022-06-29 16:19:33 +0200

    1

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-SQYflP0Igy6TY5ahlIoqSALVdflhpq0",
-  authDomain: "slack-react-11c0e.firebaseapp.com",
-  projectId: "slack-react-11c0e",
-  storageBucket: "slack-react-11c0e.appspot.com",
-  messagingSenderId: "137047571920",
-  appId: "1:137047571920:web:05e712b73280232699cdd5",
-  measurementId: "G-5739QV75HX",
+
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
