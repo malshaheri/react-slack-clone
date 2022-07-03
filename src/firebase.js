72544b25a2f9fb0826c45e75da4049eb2db2c0b2
Diala-Sleem <diala.slim@yahoo.com>
2022-07-03 23:12:52 +0200

    up

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import "firebase/compat/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZAMVf_TEvC1vkeDYm9XIbORY-_SPQXy8",
-  authDomain: "dialaslackreact.firebaseapp.com",
-  projectId: "dialaslackreact",
-  storageBucket: "dialaslackreact.appspot.com",
-  messagingSenderId: "842180155509",
-  appId: "1:842180155509:web:24df8037f5f91f5ed0003c",
-  measurementId: "G-P4MFFJ9VNR",
+
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
